ca5ec584a891bf8e69146520258437dd5d3b4134
Jakub Bartuś <141475592+BenzoEnzo@users.noreply.github.com>
2024-05-30 16:25:04 +0200

    better-wykresik-w-ł

--- regresja_logistyczna.R
+++ regresja_logistyczna.R
@@ -1,43 +1,50 @@
 library(openxlsx)
-set.seed(42)
 
-amount <- 300
+#set.seed(42)
 
-# age <- sample(20:84, amount, replace = TRUE)
+#amount <- 300
 
- # intelligence <- sample(67:130, amount, replace = TRUE)
+#age <- sample(20:84, amount, replace = TRUE)
 
- #strength <- sample(10:100, amount, replace = TRUE)
+#intelligence <- sample(67:130, amount, replace = TRUE)
 
- # healthy_diet_probability <- ifelse((intelligence*0.5 + strength * 0.53 > 75) | (age > 65), 1, 0)
+#strength <- sample(10:100, amount, replace = TRUE)
 
- # DATA_TABLE <- data.frame(age, intelligence, strength, healthy_diet_probability)
+#healthy_diet_probability <- ifelse((intelligence*0.5 + strength * 0.53 > 75) | (age > 65), 1, 0)
 
- # write.csv(DATA_TABLE, file = "logistic.csv")
+#DATA_TABLE <- data.frame(age, intelligence, strength, healthy_diet_probability)
+
+#write.csv(DATA_TABLE, file = "logistic.csv")
 
 DATA_TABLE <- read.csv("logistic.csv") 
- 
-summary(DATA_TABLE)
 
-base_model <- glm(healthy_diet_probability  ~ age + intelligence + strength, data = DATA_TABLE, family = "binomial")
+summary(DATA_TABLE)
+  
+
+base_model <- glm(healthy_diet_probability ~ age + intelligence + strength, data = DATA_TABLE, family = "binomial")
+
 summary(base_model)
+  
 
 modelIntelligence <- glm(healthy_diet_probability ~ intelligence, data = DATA_TABLE, family = "binomial")
-
 modelIntelligence_df <- data.frame(intelligence = seq(min(DATA_TABLE$intelligence), max(DATA_TABLE$intelligence), len = 300))
-
 modelIntelligence_df$healthy_diet_probability <- predict(modelIntelligence, modelIntelligence_df, type = "response")
+  
 
-plot(healthy_diet_probability ~ intelligence, data = DATA_TABLE, col = "lightgreen")
-lines(modelIntelligence_df$intelligence, modelIntelligence_df$healthy_diet_probability
-      , lwd = 2, col = "red")
+plot(healthy_diet_probability ~ intelligence, data = DATA_TABLE, col = "lightgreen",
+       main = "Healthy Diet Probability vs. Intelligence",
+       xlab = "Intelligence",
+       ylab = "Probability of Healthy Diet")
+  lines(modelIntelligence_df$intelligence, modelIntelligence_df$healthy_diet_probability, lwd = 2, col = "red")
+  
 
 modelStrength <- glm(healthy_diet_probability ~ strength, data = DATA_TABLE, family = "binomial")
-
 modelStrength_df <- data.frame(strength = seq(min(DATA_TABLE$strength), max(DATA_TABLE$strength), len = 300))
-
 modelStrength_df$healthy_diet_probability <- predict(modelStrength, modelStrength_df, type = "response")
-
-plot(healthy_diet_probability ~ strength, data = DATA_TABLE, col = "lightgreen")
-
-lines(modelStrength_df$strength, modelStrength_df$healthy_diet_probability, lwd = 2, col = "blue")
+  
+ 
+plot(healthy_diet_probability ~ strength, data = DATA_TABLE, col = "lightgreen",
+       main = "Healthy Diet Probability vs. Strength",
+       xlab = "Strength",
+       ylab = "Probability of Healthy Diet")
+  lines(modelStrength_df$strength, modelStrength_df$healthy_diet_probability, lwd = 2, col = "blue")
